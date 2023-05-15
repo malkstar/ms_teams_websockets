714cc3b7b46db1e9fe6cc6d5deb64989dc4620fa
Jim <malkstar@users.noreply.github.com>
2023-05-15 20:43:22 +0000

    Add possible actions to const.py

--- custom_components/ms_teams_websockets/const.py
+++ custom_components/ms_teams_websockets/const.py
@@ -1,3 +1,25 @@
 """Constants for the MS Teams Websockets integration."""
 
-DOMAIN = "ms_teams_websockets"
+DOMAIN: str = "ms_teams_websockets"
+
+ACTION_GET_UPDATE: dict = {
+    "service": "query-meeting-state",
+    "action": "query-meeting-state",
+}
+ACTION_TOGGLE_MUTE: dict = {"service": "toggle-mute", "action": "toggle-mute"}
+ACTION_TOGGLE_VIDEO: dict = {"service": "toggle-video", "action": "toggle-video"}
+ACTION_LEAVE_MEETING: dict = {"service": "call", "action": "leave-call"}
+ACTION_BLUR_BACKGROUND: dict = {
+    "service": "background-blur",
+    "action": "toggle-background-blur",
+}
+ACTION_RAISE_HAND: dict = {"service": "raise-hand", "action": "toggle-hand"}
+ACTION_REACT_APPLAUSE: dict = {"service": "call", "action": "react-applause"}
+ACTION_REACT_LAUGH: dict = {"service": "call", "action": "react-laugh"}
+ACTION_REACT_LIKE: dict = {"service": "call", "action": "react-like"}
+ACTION_REACT_LOVE: dict = {"service": "call", "action": "react-love"}
+ACTION_BASE_MESSAGE: dict = {
+    "apiVersion": "1.0.0",
+    "manufacturer": "Elgato",
+    "device": "StreamDeck",
+}
